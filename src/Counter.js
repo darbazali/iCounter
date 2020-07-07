ca708db63c2e5351b1fc94f7a4f2feec9c512c69
Darbaz Ali <darbaz.a.h@gmail.com>
2020-07-07 11:23:11 +0300

    move to hooks

--- src/Counter.js
+++ src/Counter.js
@@ -1,28 +1,54 @@
-import React, { useState } from 'react'
+import React from 'react';
+import { Provider, useSelector, useDispatch } from 'react-redux';
+import store from './store';
+
+// Acction Names
+export const INCREMENT = 'INCREMENT', DECREMENT = 'DECREMENT', RESET = 'RESET';
 
 
+function CounterApp() {
+  // counter state
+  const count = useSelector(state => state)
 
-export default function Counter() {
-  const [count, setCount] = useState(0);
+  // dispatcher
+  const dispatch = useDispatch()
+
 
   function increment() {
-    setCount( count + 1)
+    dispatch({
+      type: INCREMENT
+    })
   }
 
   function decrement() {
-    setCount( count - 1)
+    dispatch({
+      type: DECREMENT
+    })
   }
 
   function reset() {
-    setCount(0)
+    dispatch({
+      type:RESET
+    })
   }
+
   return (
     <div id="counter">
       <p>Count your moments</p>
       <h1>{count}</h1>
-      <button onClick={decrement}>-</button>
-      <button onClick={reset}>RESET</button>
-      <button onClick={increment}>+</button>
+      <button className="btn" onClick={decrement}>-</button>
+      <button className="btn" onClick={reset}>RESET</button>
+      <button className="btn" onClick={increment}>+</button>
     </div>
+  )
+}
+
+
+
+export default function Counter() {
+  return (
+    <Provider store={store}>
+      <CounterApp />
+    </Provider>
   )
 }
